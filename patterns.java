72a357ed9692d7099db5b1e31852a1ba309e1748
Rashim-08 <rashimanvar2005@gmail.com>
2026-04-13 22:53:19 +0530

    dsa practice repo

---
+++ patterns.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+public class patterns {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("enter the length of patter: ");
+        int n=sc.nextInt();
+
+        // pattern1
+        // for(int i=0;i<n;i++){
+        //    for(int j=0;j<n;j++)
+        //    { System.out.print("*");}
+        //    System.out.println();
+        // }
+
+        // pattern2
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // pattern3
+        //  for(int i=0;i<n;i++){
+        //     for(int j=1;j<=i+1;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+         
+        // pattern4
+        //  for(int i=1;i<=n;i++){
+        //     for(int j=0;j<i;j++){
+        //         System.out.print(i);
+        //     }
+        //     System.out.println();
+        // }
+        
+        // pattern5
+        //  for(int i=n;i>0;i--){
+        //     for(int j=i;j>0;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // pattern6
+        //  for(int i=n;i>0;i--){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+    
+        // pattern7
+        // for(int i=0;i<n;i++){
+        //     for(int j=n;j>0;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<2*i+1;j++){
+        //         System.out.print("*");
+        //     }
+        //     n--;
+        //     System.out.println();
+        // }
+    int k=n;
+        // pattern8
+        // for(int i=0;i<=n;i++){
+        //     for(int j=0;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<2*k+1;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //     k--;
+        // }
+    
+    
+    }
+}
