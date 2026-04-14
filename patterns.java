7ca62409b03f9ab6163af552766a27e97d9b22f4
Rashim-08 <rashimanvar2005@gmail.com>
2026-04-14 15:47:05 +0530

    patterns added

--- patterns.java
+++ patterns.java
@@ -1,11 +1,10 @@
-
 import java.util.Scanner;
 public class patterns {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.print("enter the length of patter: ");
         int n=sc.nextInt();
-
+int k=n;
         // pattern1
         // for(int i=0;i<n;i++){
         //    for(int j=0;j<n;j++)
@@ -64,7 +63,7 @@ public class patterns {
         //     n--;
         //     System.out.println();
         // }
-    int k=n;
+    
         // pattern8
         // for(int i=0;i<=n;i++){
         //     for(int j=0;j<=i;j++){
@@ -76,7 +75,57 @@ public class patterns {
         //     System.out.println();
         //     k--;
         // }
-    
-    
+        
+        // pattern 9
+        // for(int i=0;i<n;i++){
+        //     for(int j=k;j>0;j--){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=0;j<2*i+1;j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //     k--;
+        // }
+        // k=n-1;
+        // for(int i=0;i<=n;i++){
+        //      for(int j=0;j<=i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=2*k+1;j>0;j--){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        //     k--;
+        // }
+        
+        // pattern 10
+        // for(int i=0;i<=2*n-1;i++){
+        //     if(i<=n){
+        //         for(int j=0;j<=i;j++){
+        //         System.out.print("*");
+        //         }
+        //         System.out.println();
+        //     }
+        //     if(i>=n){
+        //         for(int j=k;j>0;j--){
+        //             System.out.print("*");
+        //         }
+        //         System.out.println();
+        //         k--;
+        //     }
+        // }
+
+        // pattern 11
+    int l=1;
+        for(int i=0;i<n;i++){
+        
+            for(int j=0;j<=i;j++){
+               l=l^1;
+                System.out.print(l);
+            }
+            System.out.println();
+        }
+
     }
 }
