e24998e2536916580b04b9ca1e8f40d1f03a5795
Rashim-08 <rashimanvar2005@gmail.com>
2026-05-04 19:50:55 +0530

    heapsort

--- sorting/heapsort.java
+++ sorting/heapsort.java
@@ -1,40 +1,42 @@
 
-class Heapsort{
+class H{
     int []a;
     int n;
-    Heapsort(int[] a){
-        n=a.length;
-        this.a=a;
-    
+
+    H(int[] a){
+        this.a = a;
+        n = a.length;
     }
     void heapify(int[] a,int n,int i){
-        int parent=i;
-        int l=2*i+1;
-        int r=2*i+2;
+        int largest = i;
+        int l = 2 * i + 1;
+        int r = 2 * i + 2;
 
-        if(l<n && a[l] > a[parent])
-            parent=l;
-        if(r<n && a[r] > a[parent])
-            parent=r;
-        if(parent != i){
-            a[i]=a[i]+a[parent];
-            a[parent]=a[i]-a[parent];
-            a[i]=a[i]-a[parent];
-            heapify(a,n,parent);
+        if (l<n && a[l] > a[largest])
+            largest = l;
+
+        if (r<n && a[r] > a[largest])
+            largest = r;
+        if (largest != i){
+            int temp = a[i];
+            a[i] = a[largest];
+            a[largest] = temp;
+    
+            heapify(a,n,largest);
         }
     }
     void sort(){
-        for(int i=n/2-1;i>=0;i--){
+        for (int i = n/2-1 ; i>=0; i--){
             heapify(a,n,i);
         }
-        for(int i=n-1;i>0;i--){
-            a[0]=a[0]+a[i];
-            a[i]=a[0]-a[i];
-            a[0]=a[0]-a[i];
+        for (int i = n-1; i>0 ; i--){
+           int temp = a[0]; 
+           a[0] = a[i];
+           a[i] = temp;
             heapify(a,i,0);
         }
         System.out.print("[");
-        for(int i=0;i<n;i++){
+        for(int i = 0; i<n ; i++){
             System.out.print(a[i]);
             if(i<n-1)System.out.print(",");
         }
@@ -46,7 +48,7 @@ class Heapsort{
 public class heapsort {
     public static void main(String[] args) {
         int []a={7,2,3,1,6,8};
-        Heapsort h=new Heapsort(a);
-        h.sort();q
+        H h=new H(a);
+        h.sort();
     }
 }
